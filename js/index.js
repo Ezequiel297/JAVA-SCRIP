ecacfac86f0395aa7e12e9c0b239d81c3dcf2e4f
ezequiel297 <mezequiel297@gmail.com>
2024-01-29 23:58:15 -0300

    inicio de git

---
+++ js/index.js
@@ -0,0 +1,52 @@
+//estructura de una variable
+
+//let nombreDeLaVariable = valor;
+
+let NumeroUno = 1
+
+console.log(NumeroUno); 
+
+//operaciones aritmeticas
+
+let NumeroTres = 10
+let numeroDos = 19
+
+let suma = NumeroTres + numeroDos
+console.log(suma);
+
+let nombre = prompt("ingrese su nombre");
+let apellido = prompt("ingrese su apellido");
+
+console.log(nombre)
+console.log (apellido)  
+
+let NombreCompleto = nombre + " " + apellido
+
+console.log("bienvenido señor/a " + NombreCompleto)
+
+let variable1 = 2;
+let variable2 = "2";
+
+if(variable1 == variable2){
+    console.log("son lo mismo para este operador")
+}
+
+let numero = prompt("ingresa tu numero")
+
+if(numero > 2){
+    console.log("numero positivo")
+}else if(numero < 2){
+    console.log("numero negativo");
+}else{
+    console.log("numero igual");
+}
+
+
+const EDAD = prompt("ingresa tu edad");
+
+if(EDAD >= 18){
+    console.log("podes manejar papichulo")
+}else{
+    console.log("que haces aca papichulo?")
+}
+
